8c349f98c90059f559908f2440df9d0fd85a5c23
LucasFelipeMartins <lucasfelipemartins789@gmail.com>
2025-08-22 20:02:36 -0300

    começando a introduzir interface grafica

--- src/main/java/com/mycompany/projetobiblioteca/ProjetoBiblioteca.java
+++ src/main/java/com/mycompany/projetobiblioteca/ProjetoBiblioteca.java
@@ -15,45 +15,14 @@ import model.dao.IDaoPessoa;
 import java.util.List;
 import model.avaliacao.AvaliacaoLivro;
 import model.recomendacao.RecomendacaoLivros;
+import view.gui.FrHome;
 
 public class ProjetoBiblioteca {
 
     public static void main(String[] args) {
 
-     
-        // ===== Criando livros =====
-        Livro livro1 = new Livro("O Pequeno Príncipe", Arrays.asList("Antoine de Saint-Exupéry"), 1943, null, "Infantil");
-        livro1.addAvaliacao(4.0);
-        livro1.addAvaliacao(5.0);
-
-        Livro livro2 = new Livro("1984", Arrays.asList("George Orwell"), 1949, null, "Distopia");
-        livro2.addAvaliacao(4.0);
-        livro2.addAvaliacao(3.0);
-
-        Livro livro3 = new Livro("Harry Potter", Arrays.asList("J.K. Rowling"), 1997, null, "Fantasia");
-        livro3.addAvaliacao(5.0);
-
-        Livro livro4 = new Livro("Dom Casmurro", Arrays.asList("Machado de Assis"), 1899,null, "Romance");
-        livro4.addAvaliacao(4.0);
-
-        List<Livro> todosLivros = Arrays.asList(livro1, livro2, livro3, livro4);
-
-        // ===== Criando usuários =====
-        Usuario usuario1 = new Usuario(1, "Lucas", "123456789", Arrays.asList(livro1), Arrays.asList("Distopia", "Fantasia"));
-        Usuario usuario2 = new Usuario(2, "Mariana", "987654321", Arrays.asList(), Arrays.asList("Romance", "Infantil"));
-
-        // ===== Gerando recomendações para Lucas =====
-        RecomendacaoLivros recomendacaoLucas = new RecomendacaoLivros(usuario1, todosLivros);
-        List<Livro> livrosRecomendadosLucas = recomendacaoLucas.gerarRecomendacoes();
-
-        System.out.println("Recomendações para " + usuario1.getNome() + ":");
-        livrosRecomendadosLucas.forEach(l -> System.out.println("- " + l.getTitulo()));
-
-        // ===== Gerando recomendações para Mariana =====
-        RecomendacaoLivros recomendacaoMariana = new RecomendacaoLivros(usuario2, todosLivros);
-        List<Livro> livrosRecomendadosMariana = recomendacaoMariana.gerarRecomendacoes();
-
-        System.out.println("\nRecomendações para " + usuario2.getNome() + ":");
-        livrosRecomendadosMariana.forEach(l -> System.out.println("- " + l.getTitulo()));
+        FrHome tela = new FrHome();
+        tela.setVisible(true);
+       
     }
 }
---
+++ src/main/java/view/gui/FrHome.java
@@ -0,0 +1,101 @@
+package view.gui;
+
+import view.dialogs.DlgCadUsuario;
+
+public class FrHome extends javax.swing.JFrame {
+
+    public FrHome() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        btnCadUsuario = new javax.swing.JButton();
+        btnCadAutor = new javax.swing.JButton();
+        btnCadLivro = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        btnRecomendacao = new javax.swing.JButton();
+        btnAddLivro = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel1.setText("BIBLIOTECA");
+        jPanel1.add(jLabel1);
+
+        btnCadUsuario.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
+        btnCadUsuario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/avatar-de-perfil.png"))); // NOI18N
+        btnCadUsuario.setText("CAD DE USUARIO");
+        btnCadUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCadUsuarioActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnCadUsuario);
+
+        btnCadAutor.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
+        btnCadAutor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/homem-de-negocios.png"))); // NOI18N
+        btnCadAutor.setText("CAD DE AUTOR");
+        jPanel2.add(btnCadAutor);
+
+        btnCadLivro.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
+        btnCadLivro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/abra-o-livro.png"))); // NOI18N
+        btnCadLivro.setText("CAD DE LIVRO");
+        jPanel2.add(btnCadLivro);
+
+        btnRecomendacao.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnRecomendacao.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/pilha-de-livros.png"))); // NOI18N
+        btnRecomendacao.setText("SISTEMA DE RECOMENDAÇÔES");
+        jPanel3.add(btnRecomendacao);
+
+        btnAddLivro.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnAddLivro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/livros.png"))); // NOI18N
+        btnAddLivro.setText("ADCIONAR LIVRO LIDO");
+        jPanel3.add(btnAddLivro);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(37, 37, 37)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 302, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCadUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadUsuarioActionPerformed
+        DlgCadUsuario tela = new DlgCadUsuario(this, true);
+        tela.setVisible(true);
+    }//GEN-LAST:event_btnCadUsuarioActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAddLivro;
+    private javax.swing.JButton btnCadAutor;
+    private javax.swing.JButton btnCadLivro;
+    private javax.swing.JButton btnCadUsuario;
+    private javax.swing.JButton btnRecomendacao;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    // End of variables declaration//GEN-END:variables
+}
